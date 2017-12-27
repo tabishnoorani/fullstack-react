6d3e3f7d78939415af13f47470647feae4de7492
Tabbish Noorani <tabishnoornai@gmail.com>
2017-12-27 21:34:48 +1000

    Added express and common middleware. Configure the basic server

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import config from '../config';
 import mongoose from 'mongoose';
 
-const url = "mongodb://localhost:27017";
+const url = "mongodb://localhost:27017/imrego";
 
 mongoose.connect(url, {useMongoClient: true},  (err)=>{
     console.log (err?err:"Connected")
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './database/index';
+import config from './config'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import './database';
+
+const server = express();
+
+server.use(express.static('public'));
+server.use(bodyParser.urlencoded({extended:true}));
+// server.use(bodyParser.json);
+
+server.set('view-engine','ejs');
+
+server.get("/ejs", (req, res)=>{
+   res.render('index.ejs', {config: config.PORT})
+});
+
+server.listen(config.PORT, (err)=>{
+    console.log (err?err:`Server is listening on port: ${config.PORT}`)
+});
